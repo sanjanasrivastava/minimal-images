fadb39e3ab851ebeb0f70b1cc3589b544a4c5f35
Sanjana <sanjanas@mit.edu>
2018-04-26 22:08:51 -0400

    debug

--- minimal-image-statistics.py
+++ minimal-image-statistics.py
@@ -293,7 +293,7 @@ def get_all_correctness(model_name):
             batch = images[counter:min(counter + BATCH_SIZE, settings.SMALL_DATASET_SIZE)]      # get the next BATCH_SIZE images (or until the end)
             counter += BATCH_SIZE                                                               # next step will start from +BATCH_SIZE
             if counter > settings.SMALL_DATASET_SIZE:                                           # if batch is smaller than BATCH_SIZE...
-                num_stubs = counter - len(batch)
+                num_stubs = BATCH_SIZE - len(batch)
                 batch.extend([batch[-1] for __ in range(num_stubs)])                            # extend the last crop into num_stubs spots
             prob = sess.run(network.probs, feed_dict={network.imgs: batch})
             print(prob)
