d95b3a82854a4ec9808960722e6bb05777981540
Sanjana <sanjanas@mit.edu>
2018-04-30 17:49:54 -0400

    debugging

--- minimal-image-statistics.py
+++ minimal-image-statistics.py
@@ -341,7 +341,8 @@ def crop_correctness_in_bbx(crop_metric, model_name, image_scale):
 
     all_img_pct_correct_in_bbx = {}
     for smalldataset_id in range(settings.SMALL_DATASET_SIZE):
-        if smalldataset_id in intractable_images:
+        if not os.path.exists(PATH_TO_DATA + settings.map_filename(settings.TOP5_MAPTYPE, crop_metric, model_name, image_scale)):
+            print(smalldataset_id)
             continue
         top5map = np.load(PATH_TO_DATA + settings.map_filename(settings.TOP5_MAPTYPE, crop_metric, model_name, image_scale, smalldataset_id) + '.npy')
         bbx_dims = settings.get_bbx_dims(all_bbxs, smalldataset_id)
