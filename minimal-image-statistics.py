7b6eff7e1b90577c611bd6092e8ee58cbe543d0d
Sanjana <sanjanas@mit.edu>
2018-05-08 00:53:40 -0400

    debugging inception

--- minimal-image-statistics.py
+++ minimal-image-statistics.py
@@ -386,7 +386,8 @@ def test_get_all_correctness2(model_name):
     for i in range(len(ids)):
       im_id = ids[i]
       prob = probabilities[i]
-      settings.get_class_labels([settings.convert_id_small_to_imagenetval(ids[i])])
+      # settings.get_class_labels([settings.convert_id_small_to_imagenetval(ids[i])])
+      print('TRUE LABEL:', true_labels[i])
       inds = np.argsort(prob)[::-1]
       print(inds[0:5])
       for j in range(5):
