fec88817db36441464852f8da8bdb7d3b8347654
Sanjana <sanjanas@mit.edu>
2018-05-15 13:34:37 -0400

    debugging inception accuracy

--- minimal-image-statistics.py
+++ minimal-image-statistics.py
@@ -311,6 +311,9 @@ def get_all_correctness(model_name):
     print('IMAGE TEST ACCURACY:', sum(all_correctness[i][0] for i in ids) / float(len(ids)))
     print('BBX TEST ACCURACY:', sum(all_correctness[i][1] for i in ids) / float(len(ids)))
 
+    with open(PATH_TO_OUTPUT_DATA + model_name + '-small-dataset-classification-correctness.json', 'w') as writefile:
+        json.dump(all_correctness, writefile)
+
 
 def get_all_correctness2(model_name):
 
