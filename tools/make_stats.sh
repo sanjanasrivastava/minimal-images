7d95dd3cd298a22c68c4a226e26f51f2a5ecdf8c
Sanjana <sanjanas@mit.edu>
2018-05-16 01:36:12 -0400

    fixing id-to-measurements

--- tools/make_stats.sh
+++ tools/make_stats.sh
@@ -7,7 +7,8 @@
 #SBATCH --workdir=./log/
 #SBATCH --qos=use-everything
 
-declare -a CROP_METRICS=("0.2" "0.4" "0.6" "0.8")
+# declare -a CROP_METRICS=("0.2" "0.4" "0.6" "0.8")
+declare -a CROP_METRICS=("0.4" "0.6" "0.8")
 declare -a MODELS=("vgg16" "resnet" "inception")
 declare -a IMAGE_SCALES=("1")
 declare -a STRICTNESSES=("loose" "strict")
@@ -18,7 +19,7 @@ for CROP_METRIC in "${CROP_METRICS[@]}"; do
     for MODEL in "${MODELS[@]}"; do
         for IMAGE_SCALE in "${IMAGE_SCALES[@]}"; do
             for STRICTNESS in "${STRICTNESSES[@]}"; do
-                for AXIS in "${AXES[@]}"; do 
+                for AXIS in "${AXES[@]}"; do
                      echo "$CROP_METRIC $MODEL $IMAGE_SCALE $STRICTNESS $AXIS"
                      singularity exec -B /om:/om -B /cbcl:/cbcl --nv /om/user/sanjanas/belledon-tensorflow-keras-master-latest.simg \
                      python minimal-image-statistics.py $CROP_METRIC $MODEL $IMAGE_SCALE $STRICTNESS $AXIS
