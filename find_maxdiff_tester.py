cc4125817889228f7d2cb20d347dfa9922dc6f05
Sanjana <sanjanas@mit.edu>
2018-05-24 22:05:49 -0400

    generating more minimal images

--- find_maxdiff_tester.py
+++ find_maxdiff_tester.py
@@ -149,7 +149,7 @@ def save_crops(coords, crop_metric, model_name, image_scale, axis, compare_corr,
     if not os.path.exists(folder):
         os.makedirs(folder)
 
-    top_ids = sorted(coords, reverse=True, key=lambda x: coords[x][2])[:num_samples]
+    top_ids = sorted(coords, reverse=True, key=lambda x: coords[x][2])[num_samples:num_samples * 2]
 
     for smalldataset_id in top_ids:
 
