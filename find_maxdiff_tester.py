a12eba02238f435cac53bebbc3d863c59c250ce3
Sanjana <sanjanas@mit.edu>
2018-09-24 13:22:13 -0400

    debug

--- find_maxdiff_tester.py
+++ find_maxdiff_tester.py
@@ -318,7 +318,8 @@ if __name__ == '__main__':
     # print('COMPLETED SCRIPT')
     sample_ids_resnet_large = [363, 220, 162, 151, 474, 203, 176, 469, 112, 401, 389, 452, 45, 342, 206, 134, 166, 200, 242, 58, 54, 460, 350, 319, 292, 237, 433, 352, 21, 344]
     sample_ids_vgg_small = [208, 176, 489, 193, 385, 247, 254, 241, 314, 356, 125, 280, 192, 322, 57, 133, 181, 366, 293, 203, 72, 325, 237, 93, 216, 329, 376, 408, 270, 42]
-    for small_id in sample_ids_vgg_small:
+    sample_ids_inception_small = [61, 122, 286, 289, 384, 437, 481, 258, 175, 312, 376, 118, 401, 101, 77, 487, 102, 200, 471, 10, 496, 216, 405, 443, 295, 489, 103, 148, 87, 125]
+    for small_id in sample_ids_inception_small:
         coords = get_maxdiff_coordinates(small_id, small_id, 0.2, 'vgg16', 1.0, 'scale')
         save_crops(coords, 0.2, 'vgg16', 1.0, 'scale', True)
 
